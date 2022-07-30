0399b68a19422ec6ce982f34f526f091cd865c90
Vilayat Kleer <vkleer@hotmail.com>
2022-07-30 17:50:12 +0000

    Change board_marks from list to dictionary, change game_board function to show 5x5 grid instead of 3x3 grid

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-board_marks = [' ', ' ', ' ',
-               ' ', ' ', ' ',
-               ' ', ' ', ' ']
+board_marks = {
+    'row-1': [' ', ' ', ' ', ' ', ' '],
+    'row-2': [' ', ' ', ' ', ' ', ' '],
+    'row-3': [' ', ' ', ' ', ' ', ' '],
+    'row-4': [' ', ' ', ' ', ' ', ' '],
+    'row-5': [' ', ' ', ' ', ' ', ' ']}
+
 
 
 def game_board(marks):
@@ -8,18 +12,22 @@ def game_board(marks):
     Prints the gameboard to the console
     """
 
-    print('┌─────┬─────┬─────┐')
-    print(f'│  {marks[0]}  │  {marks[1]}  │  {marks[2]}  │')
-    print('├─────┼─────┼─────┤')
-    print(f'│  {marks[3]}  │  {marks[4]}  │  {marks[5]}  │')
-    print('├─────┼─────┼─────┤')
-    print(f'│  {marks[6]}  │  {marks[7]}  │  {marks[8]}  │')
-    print('└─────┴─────┴─────┘')
+    print('┌─────┬─────┬─────┬─────┬─────┐')
+    print(f"│  {marks['row-1'][0]}  │  {marks['row-1'][1]}  │  {marks['row-1'][2]}  │  {marks['row-1'][3]}  │  {marks['row-1'][4]}  │")
+    print('├─────┼─────┼─────┼─────┼─────┤')
+    print(f"│  {marks['row-2'][0]}  │  {marks['row-2'][1]}  │  {marks['row-2'][2]}  │  {marks['row-2'][3]}  │  {marks['row-2'][4]}  │")
+    print('├─────┼─────┼─────┼─────┼─────┤')
+    print(f"│  {marks['row-3'][0]}  │  {marks['row-3'][1]}  │  {marks['row-3'][2]}  │  {marks['row-3'][3]}  │  {marks['row-3'][4]}  │")
+    print('├─────┼─────┼─────┼─────┼─────┤')
+    print(f"│  {marks['row-4'][0]}  │  {marks['row-4'][1]}  │  {marks['row-4'][2]}  │  {marks['row-4'][3]}  │  {marks['row-4'][4]}  │")
+    print('├─────┼─────┼─────┼─────┼─────┤')
+    print(f"│  {marks['row-5'][0]}  │  {marks['row-5'][1]}  │  {marks['row-5'][2]}  │  {marks['row-5'][3]}  │  {marks['row-5'][4]}  │")
+    print('└─────┴─────┴─────┴─────┴─────┘')
 
 
-def game_menu():
+def main_menu():
     """
-    Shows the menu to users, allowing them to start the game
+    Shows the menu to users, giving them the option to start the game
     or read the game instructions
     """
     print('Welcome to Tic-Tac-Toe!\n')
@@ -37,5 +45,6 @@ def game_menu():
     elif menu_input == '2':
         print('Load game instructions')
 
-game_menu()
-# game_board(board_marks)
+
+# main_menu()
+game_board(board_marks)
