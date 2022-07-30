bd3e910c25231b6562eb738b903fb31c917082e9
Vilayat Kleer <vkleer@hotmail.com>
2022-07-30 16:59:55 +0000

    Add game_menu function to print 'Start game' or 'Load game instructions' based on user input

--- run.py
+++ run.py
@@ -17,4 +17,25 @@ def game_board(marks):
     print('└─────┴─────┴─────┘')
 
 
-game_board(board_marks)
+def game_menu():
+    """
+    Shows the menu to users, allowing them to start the game
+    or read the game instructions
+    """
+    print('Welcome to Tic-Tac-Toe!\n')
+    print('Would you like to start the game or read the game instructions?')
+    menu_options = '1. Start the game\n2. Read game instructions\n'
+    menu_input = input(menu_options)
+
+    while menu_input != '1' and menu_input != '2':
+        print(f'\nYou entered {menu_input}, which is not a valid option.\n')
+        print('Please select one of the two options:')
+        menu_input = input(menu_options)
+
+    if menu_input == '1':
+        print('Start game')
+    elif menu_input == '2':
+        print('Load game instructions')
+
+game_menu()
+# game_board(board_marks)
