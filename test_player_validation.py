b21256c62316d3d372e7c51ab4bfa78a8411d6af
Vilayat Kleer <vkleer@hotmail.com>
2022-08-03 20:16:39 +0000

    Add validate_player_username test from player_validation.py to test_player_validation.py

--- test_player_validation.py
+++ test_player_validation.py
@@ -3,6 +3,11 @@ import player_validation as play_val
 
 
 class TestUsernameAndEmailValidation(unittest.TestCase):
+    def test_validate_player_username(self):
+        self.assertTrue(
+            play_val.validate_player_username('Billy123'), True
+        )
+
     def test_validate_player_email(self):
         self.assertTrue(
             play_val.validate_player_email('test@outlook.com'), True
