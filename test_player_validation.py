e6fa7c49ccd387c6d13b5bd5d86fccbac2791c29
Vilayat Kleer <vkleer@hotmail.com>
2022-08-03 20:12:39 +0000

    Create test_player_validation.py and add TestUsernameAndEmailValidation class, which for now only tests validate_player_email function from player_validation.py

---
+++ test_player_validation.py
@@ -0,0 +1,12 @@
+import unittest
+import player_validation as play_val
+
+
+class TestUsernameAndEmailValidation(unittest.TestCase):
+    def test_validate_player_email(self):
+        self.assertTrue(
+            play_val.validate_player_email('test@outlook.com'), True
+        )
+
+if __name__ == '__main__':
+    unittest.main()
